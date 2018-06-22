76d49dd996345c92e6aa6931ac487a260d0f803a
Jason Tu <jason_tu@puyacn.com>
2018-06-22 14:04:10 +0800

    add rpc mode: preforking process

---
+++ trpc/rpc_client.py
@@ -0,0 +1,47 @@
+import os
+import json
+import struct
+import socket
+import asyncio
+
+
+async def rpc(sock, in_, params):
+    request = json.dumps({'in': in_, 'params': params})
+    len_prefix = struct.pack('I', len(request))
+    sock.send(len_prefix)
+    sock.sendall(request.encode())
+    len_prefix = sock.recv(4)
+    print(len_prefix)
+    length, = struct.unpack('I', len_prefix)
+    body = sock.recv(length)
+    response = json.loads(body.decode())
+    return response
+
+
+def main(t_num):
+    loop = asyncio.get_event_loop()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('192.168.1.195', 8080))
+    pid = os.getpid()
+    tasks = []
+    for item in range(t_num):
+        task = asyncio.ensure_future(rpc(s, 'ping', '{}.{}'.format(pid, item)))
+        tasks.append(task)
+
+    loop.run_until_complete(asyncio.wait(tasks))
+
+
+def prefork(p_num):
+    for i in range(p_num):
+        pid = os.fork()
+        if pid < 0:
+            return
+        if pid > 0:
+            continue
+        if pid == 0:
+            break
+
+
+if __name__ == '__main__':
+    prefork(50)
+    main(100)
