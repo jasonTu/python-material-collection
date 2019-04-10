720afda023ddfdd46f50d08faf4bbd78d111a028
Jason Tu <jason_tu@trendmicro.com>
2019-04-10 10:48:43 -0400

    add django_cron practice

---
+++ examples/django_cron_test/testcron/cronapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CronappConfig(AppConfig):
+    name = 'cronapp'
---
+++ examples/django_cron_test/testcron/cronapp/cron.py
@@ -0,0 +1,11 @@
+from django_cron import CronJobBase, Schedule
+
+class MyCronJob(CronJobBase):
+    RUN_EVERY_MINS = 1
+
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    # A unique code
+    code = 'my_app.my_cron_job'
+
+    def do(self):
+        print('I am running')
