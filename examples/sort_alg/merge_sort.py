6ba1b35ec6fad7ebf985f2f6bf7a1abaf02c68e5
Jason Tu <jason_tu@trendmicro.com>
2019-04-14 10:35:03 -0400

    add merge sort skeleton

---
+++ examples/sort_alg/merge_sort.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+'''
+As basic algorithms to be remember as a "1+1=2" to be expirenced engineer.
+Sort -> Merge Sort.
+3, 4, 5, 1, 2
+Referer:
+https://baike.baidu.com/item/%E5%BD%92%E5%B9%B6%E6%8E%92%E5%BA%8F/1639015?fr=aladdin
+https://www.cnblogs.com/chengxiao/p/6194356.html
+https://en.wikipedia.org/wiki/Merge_sort
+'''
