1164f2ea83e809a4bfa350a5874f1625bbdc0e90
Jason Tu <jason_tu@trendmicro.com>
2019-08-14 03:47:01 -0400

    update property-based testing exmaple

--- test/property_based/test_example.py
+++ test/property_based/test_example.py
@@ -1,6 +1,6 @@
 import pytest
 import hypothesis.strategies as st
-from hypothesis import given, settings, Verbosity
+from hypothesis import given, settings, Verbosity, example
 
 
 def sum(num1, num2):
@@ -17,5 +17,9 @@ def test_sum(num1, num2, expected):
 
 @settings(verbosity=Verbosity.verbose)
 @given(st.integers(), st.integers())
+@example(1, 2)
 def test_sum(num1, num2):
     assert sum(num1, num2) == num1 + num2
+    assert sum(num1, 0) == num1
+    assert sum(num1, num2) == sum(num2, num1)
+    # assert num1 <= 30
